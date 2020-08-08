6172280097990d024b7f155e9b3f758be949d57b
Tommy Ringo <ringotm@gmail.com>
2020-08-07 21:17:35 -0500

    finished

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,35 +1,24 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import mars_scraper
 
 app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
 
 # Or set inline
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
-@app.route("/scrape")
-def scraper():
-    mongo.db.mars_data.drop()
-    mars_data = mongo.db.mars_data
-    mars_facts = mars_scraper.scrape_info()
-    mars_data.update({}, mars_facts, upsert=True)
-    return redirect("/", code=302)
-
-
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    mars_data = mongo.db.collection.find_one()
+    return render_template("index.html", mars_data=mars_data)
 
 
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    mongo.db.collection.drop()
+    mars_facts = mars_scraper.scrape_info()
+    mongo.db.collection.update({}, mars_facts, upsert=True)
     return redirect("/", code=302)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Web-Scraping-Challenge
+Web app that scrapes data related to mars from various websites.
+
+Tools used:
+- Beautiful Soup
+- Splinter
+- MongoDB
+- Flask
+- HTML/Bootstrap
+
+<img src='Missions_to_Mars\Images\Final_app_pt1.PNG'>
+<img src='Missions_to_Mars\Images\Final_app_pt2.PNG'>
